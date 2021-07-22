f3ac7f5045a1a5278ce8a69faa5da5e381eca25c
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-07-22 20:29:59 +0900

    javascirpt 공부 3일차

---
+++ 증감연산자Ex1.js
@@ -0,0 +1,15 @@
+let a = 20;
+console.log(a++);// 20(21)
+console.log(a++);// 21(22)
+console.log(a++);// 22(23)
+
+let b = 5;
+console.log(b);// 5
+console.log(++b);// 6 먼저 더해진 후 출력
+console.log(++b);// 7 
+console.log(++b);// 8
+
+let c = 5;
+console.log(++c);// 6 먼저 더하고 출력
+console.log(++c);// 7
+console.log(c++);// 7(8) 기존 c값을 출력 후에 1증가
